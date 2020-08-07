d5311ea2c1257b713be7037e62caf0b37b39533a
温宇航 <wenyuhang@qinjia001.com>
2020-08-07 13:49:55 +0800

    Initial commit

---
+++ src/main/java/com/wl/many_steps/ManyStepsApplication.java
@@ -0,0 +1,13 @@
+package com.wl.many_steps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManyStepsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ManyStepsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wl/many_steps/ManyStepsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wl.many_steps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManyStepsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
